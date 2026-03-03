a9e44b2617a3e628bedc292ab03a4c741138c4d5
ROIEngineer <191797684+ROIEngineer@users.noreply.github.com>
2026-03-03 18:56:42 -0500

    Add: Home Page UI
    Downloaded dependencies for Chart.js and CountUp

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,17 @@
+import { layouts } from "chart.js";
+import { Sidebar } from "lucide-react";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: 'Harold', lastName: 'Durant' };
+  
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
         {children}
     </main>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import HeaderBox from '@/components//HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Harold' };
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access your account details and transactions."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
